7d1e14165c692a7d1ba7196bca69aac30e8749c5
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-01-11 20:40:34 -0300

    Add: message class

---
+++ src/lime/protocol/message.py
@@ -0,0 +1,31 @@
+from envelope import Envelope
+
+
+class Message(Envelope):
+
+    # content_type property
+    @property
+    def content_type(self):
+        return self.__content_type
+
+    @content_type.setter
+    def content_type(self, value):
+        if isinstance(value, str):
+            self.__content_type = value
+        else:
+            raise ValueError('content_type must be a str')
+
+    # content property
+    @property
+    def content(self):
+        return self.__content
+
+    @content.setter
+    def content(self, value):
+        self.__content = value
+
+
+class MessageListener:
+
+    @staticmethod
+    def on_message(command): return None
