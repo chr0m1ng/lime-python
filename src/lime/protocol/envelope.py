b670a2460aaa52e3097f731c20ee755e5eda5a3f
Gabriel Santos <gabrielrsantoss@icloud.com>
2021-05-07 16:18:15 -0300

    Add: docstrings

--- src/lime/protocol/envelope.py
+++ src/lime/protocol/envelope.py
@@ -3,6 +3,7 @@ from serializable import Serializable
 
 
 class Envelope(ABC, Serializable):
+    """Envelope representation."""
 
     def __init__(self, id: str, from_n: str, to: str, pp: str, metadata: dict):
         self.id = id
@@ -13,6 +14,12 @@ class Envelope(ABC, Serializable):
 
 
 class EnvelopeListener(ABC):
+    """Envelope listener callbacks."""
 
     def on_envelope(self, envelope: Envelope):
+        """Handle callback to envelope received event.
+
+        Args:
+            envelope (Envelope): the received envelope
+        """
         pass
