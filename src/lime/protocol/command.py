1c7fc64025f90fad534172a8dd98b68e247a549a
Luis Correa <luisc@take.net>
2021-05-10 17:19:50 -0300

    Add: Command class

---
+++ src/lime/protocol/command.py
@@ -0,0 +1,38 @@
+from abc import ABC
+from .command_method import CommandMethod
+from .command_status import CommandStatus
+from .envelope import Envelope
+from .reason import Reason
+
+
+class Command(ABC, Envelope):  # noqa WPS230
+    """Envelope representation."""
+
+    def __init__(self,
+                 uri: str = None,
+                 type_n: str = None,
+                 resource: str = None,
+                 method: CommandMethod = None,
+                 status: CommandStatus = None,
+                 reason: Reason = None,
+                 timeout: bool = None):
+
+        self.uri = uri
+        self.type_n = type_n
+        self.resource = resource
+        self.method = method
+        self.status = status
+        self.reason = reason
+        self.timeout = timeout
+
+
+class CommandListener(ABC):
+    """Command listener callback."""
+
+    def on_listener(self, command: Command):
+        """Handle callback to handle a received command.
+
+        Args:
+            command (Command): the received command
+        """
+        pass
