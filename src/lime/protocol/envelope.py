a40c2f4f6a2e61bd251705e19dacdefa8d6d3d79
Gabriel Santos <gabrielrsantoss@icloud.com>
2021-05-11 14:51:31 -0300

    Add: envelope type checking

--- src/lime/protocol/envelope.py
+++ src/lime/protocol/envelope.py
@@ -11,3 +11,51 @@ class Envelope(ABC, Serializable):
         self.to = to
         self.pp = pp
         self.metadata = metadata
+
+    @staticmethod
+    def is_message(envelope) -> bool:
+        """Check if a envelope is a Message.
+
+        Args:
+            envelope (Envelope): the Envelope to be checked
+
+        Returns:
+            bool: True if the given Envelope is a Message
+        """
+        return 'content' in envelope
+
+    @staticmethod
+    def is_notification(envelope) -> bool:
+        """Check if a envelope is a Notification.
+
+        Args:
+            envelope (Envelope): the Envelope to be checked
+
+        Returns:
+            bool: True if the given Envelope is a Notification
+        """
+        return 'event' in envelope
+
+    @staticmethod
+    def is_command(envelope) -> bool:
+        """Check if a envelope is a Command.
+
+        Args:
+            envelope (Envelope): the Envelope to be checked
+
+        Returns:
+            bool: True if the given Envelope is a Command
+        """
+        return 'method' in envelope
+
+    @staticmethod
+    def is_session(envelope) -> bool:
+        """Check if a envelope is a Session.
+
+        Args:
+            envelope (Envelope): the Envelope to be checked
+
+        Returns:
+            bool: True if the given Envelope is a Session
+        """
+        return 'state' in envelope
