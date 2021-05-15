9a48e18ce752c5cdd1ad0e39ce38203d754dfff9
Gabriel Santos <gabrielrsantoss@icloud.com>
2021-05-15 13:30:59 -0300

    Add: command processor

---
+++ src/lime/protocol/client/command_processor.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod
+from ..command import Command
+from ..listeners import CommandListener
+
+
+class CommandProcessor(CommandListener):
+    """Command Processor."""
+
+    @abstractmethod
+    def process_command(self, command: Command, timeout: int) -> Command:
+        """Process a Command and return the result.
+
+        Args:
+            command (Command): The Command to be processed
+            timeout (int): Timeout to process the Command
+
+        Returns:
+            Command: The result Command
+        """  # noqa: DAR202
+        pass
