10ba643aa5dd87ac084d2c81c2089b665f96862c
Gabriel Santos <gabrielrsantoss@icloud.com>
2021-05-25 13:00:12 -0300

    Fix: convert to camelCase when serializing

---
+++ src/lime_python/protocol/serializable.py
@@ -0,0 +1,78 @@
+import json
+from typing import Any
+
+from humps import camelize
+
+PRIVATE_TOKEN = '_'  # noqa: S105
+NODE_KEY_TOKEN = '_n'  # noqa: S105
+
+
+class Serializable:
+    """Serializable objects to json."""
+
+    def __str__(self) -> str:
+        """Override str representation.
+
+        Returns:
+            str: json representation as str
+        """
+        return json.dumps(self.to_json())
+
+    def __eq__(self, obj: object) -> bool:
+        """Override equal to compare serializated content.
+
+        Args:
+            obj (object): the object to compare
+
+        Returns:
+            bool
+        """
+        return isinstance(obj, Serializable) and \
+            self.to_json() == obj.to_json()
+
+    def __hash__(self) -> int:
+        """Create a hash based on serializated content.
+
+        Returns:
+            int
+        """
+        return hash(str(self))
+
+    def to_json(self) -> dict:
+        """
+        Transform class properties to json.
+
+        Returns:
+            dict: the class json representation without private properties
+        """
+        return {
+            self.normalize_key(key): self.serialize_value(value)
+            for key, value in self.__dict__.items()  # noqa: WPS110
+            if self.__should_serialize_property(key, value)
+        }
+
+    def normalize_key(self, key: str) -> str:
+        """
+        Normalize a class property name.
+
+        Args:
+            key (str): property name
+
+        Returns:
+            str: the normalized key
+        """
+        return camelize(key.replace(NODE_KEY_TOKEN, str()))
+
+    def serialize_value(self, value: Any) -> Any:
+        """Serialize a value if it's Serializable.
+
+        Args:
+            value (Any): property value
+
+        Returns:
+            Any: the serialized property
+        """
+        return value.to_json() if isinstance(value, Serializable) else value
+
+    def __should_serialize_property(self, key: str, value) -> bool:
+        return not key.startswith(PRIVATE_TOKEN) and value is not None
---
+++ src/lime_python/protocol/utilities/dict_to_class.py
@@ -0,0 +1,20 @@
+from humps import decamelize
+
+from ..serializable import Serializable
+
+NORMALIZED_KEYS = frozenset(('from', 'type'))
+
+
+class DictToClass(Serializable):
+    """Turns a dictionary into a class."""
+
+    def __init__(self, dictionary: dict, class_type: type) -> None:
+        for key, value in dictionary.items():
+            setattr(self, self.__normalize_key(key), value)
+        self.__class__ = class_type
+
+    def __normalize_key(self, key) -> str:
+        key = decamelize(key)
+        if key in NORMALIZED_KEYS:
+            key = f'{key}_n'
+        return key
